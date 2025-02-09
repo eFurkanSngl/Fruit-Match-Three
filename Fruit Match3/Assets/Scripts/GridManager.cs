5e73898185f84976981e4e55c61b4f3944d958e2
Starttera <efrknsngl@gmail.com>
2025-02-10 01:33:53 +0300

    Created Scene , created grid and spirtes

---
+++ Fruit Match3/Assets/Scripts/GridManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int _gridX;
+    [SerializeField] private int _gridY;
+    [SerializeField] private GameObject[] _gameObject;
+
+    private void Start()
+    {
+        CreateGrid();
+    }
+
+
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicked");
+    }
+
+    private void CreateGrid()
+    {
+        for (int i = 0; i < _gridX; i++)
+        {
+            for (int j = 0; j < _gridY; j++)
+            {
+
+                Vector3 pos = new Vector3(transform.position.x + i, transform.position.y + j, 0);
+                int randomIndex = Random.Range(0, _gameObject.Length);
+
+                GameObject Obj = Instantiate(_gameObject[randomIndex], pos, Quaternion.identity);
+                Obj.transform.parent = transform;
+            }
+        }
+    }
+
+#if UNITY_EDITOR_64
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < _gridX; i++)
+        {
+            for (int j = 0; j < _gridY; j++)
+            {
+                Gizmos.DrawWireCube(new Vector3(transform.position.x + i, transform.position.y + j, 0), new Vector3(1, 1, 1));
+            }
+        }
+    }
+#endif
+}
