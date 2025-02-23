df05b4d1120bebc451d83bc3dd10fef06aa7e6be
Starttera <efrknsngl@gmail.com>
2025-02-23 23:43:13 +0300

    Changed prefab and level scenes

---
+++ Fruit Match3/Assets/Scripts/UI/ExitBTN.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitBTN : UIBTN
+{
+    [SerializeField] private GameObject _closePanel;
+  
+    protected override void OnClick()
+    {
+        ClosePanel();
+    }
+    private  void ClosePanel()
+    {
+        _closePanel.SetActive(false);
+        UIBtnEvents.SettingsUIEvents?.Invoke();
+    }
+}
---
+++ Fruit Match3/Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System.Threading.Tasks;
+
+public class SettingsMenu : UIBTN
+{
+    [SerializeField] private GameObject _settingsMenu;
+    [SerializeField] private RectTransform _settingsTransform;
+    [SerializeField] private RectTransform _settingsBtnTransform;
+    [SerializeField] private float _topPosY, _middlePosY;
+    [SerializeField] private float _tweenDuration;
+    [SerializeField] private CanvasGroup _canvasGroup;
+
+    private void ShowSettingsMenu()
+    {
+        _settingsMenu.SetActive(true);
+        SettingsPanelIntro();
+    }
+    protected override void OnClick()
+    {
+        ShowSettingsMenu();
+    }
+
+    private void SettingsPanelIntro()
+    {
+        _settingsBtnTransform.DOAnchorPosX(152, _tweenDuration).SetUpdate(true);
+        _canvasGroup.DOFade(1, _tweenDuration).SetUpdate(true);
+        _settingsTransform.DOAnchorPosY(_middlePosY, _tweenDuration).SetUpdate(true);
+    }
+
+    private void SettingsPanelOutro()
+    {
+        _canvasGroup.DOFade(0,_tweenDuration).SetUpdate(true);
+        _settingsBtnTransform.DOAnchorPosX(-197,_tweenDuration).SetUpdate(true);
+       _settingsTransform.DOAnchorPosY(_topPosY, _tweenDuration).SetUpdate(true);
+    }
+
+    private void OnEnable()
+    {
+        UIBtnEvents.SettingsUIEvents += SettingsPanelOutro;
+    }
+    private void OnDisable()
+    {
+        UIBtnEvents.SettingsUIEvents -= SettingsPanelOutro;
+    }
+}
