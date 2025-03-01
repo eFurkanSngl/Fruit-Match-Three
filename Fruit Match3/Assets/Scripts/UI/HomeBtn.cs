41dc7248904b807252545136f525db873a5f323b
Starttera <efrknsngl@gmail.com>
2025-03-01 23:26:21 +0300

    Level design new adjust  and fixed bug

---
+++ Fruit Match3/Assets/Scripts/UI/HomeBtn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeBtn : UIBTN
+{
+    protected override void OnClick()
+    {
+        OnLoadScene();
+    }
+
+    private void OnLoadScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
