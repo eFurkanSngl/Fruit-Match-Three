83cb137ee4630fd7c68ac59b9feaeacea02133dc
Starttera <efrknsngl@gmail.com>
2025-04-05 22:32:06 +0300

    New fond added and Scene design adjusting

--- Fruit Match3/Assets/Scripts/UI/NextLevel.cs
+++ Fruit Match3/Assets/Scripts/UI/NextLevel.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NextLevel : UIBTN
 {
+    [SerializeField] private Slider _timeSlider;
     protected override void OnClick()
     {
         GoToLevelSelect();
@@ -12,5 +14,6 @@ public class NextLevel : UIBTN
     private void GoToLevelSelect()
     {
         SceneManager.LoadScene("Level-Select");
+        _timeSlider.animator.speed = 1.0f;
     }
 }
